cb6a972a7464f3bf87c54304c810b3c59cfbe216
mefinst <mefinst@yandex.ru>
2022-08-28 23:04:02 +0300

    Revert "fail ci"
    This reverts commit b7c0e047f09282cf233efca35d588b245d5c5b58.

--- src/test/kotlin/com/mefinst/ApplicationTest.kt
+++ src/test/kotlin/com/mefinst/ApplicationTest.kt
@@ -26,7 +26,6 @@ class ApplicationTest {
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
-            assertEquals("fail", "success")
         }
     }
 }
